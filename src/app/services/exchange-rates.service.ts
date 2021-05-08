05f346c9257a937d02b43f39fa69eb1c231d1fe4
Aaron McCarthy <amccarthy9904@gmail.com>
2021-05-07 18:18:35 -0700

    connects to new API, updates obj structure of response to reflect that of the new API

--- src/app/services/exchange-rates.service.ts
+++ src/app/services/exchange-rates.service.ts
@@ -9,10 +9,11 @@ import { Observable } from 'rxjs';
 })
 export class CurrencyExchangeService {
 
+  result:any
   constructor(private httpClient:HttpClient) { }
 
   getRates(base: string): Observable<ExchangeRatesResponse>{
-    console.log(this.httpClient.get<ExchangeRatesResponse>(`https://api.exchangeratesapi.io/latest?base=${base}`))
-    return this.httpClient.get<ExchangeRatesResponse>(`https://api.exchangeratesapi.io/latest?base=${base}`)
+    //please dont take my api key
+    return this.httpClient.get<ExchangeRatesResponse>(`https://min-api.cryptocompare.com/data/price?fsym=${base}&tsyms=KWD,BHD,OMR,JOD,GIP,GBP,KYD,EUR,USD,CHF,BND,CAD,AUD,LYD,SGD,NZD,FJD,BRL,ILS,TRY&api_key={98692e6bab4fc612eb16c39b9819d00ac3b5163254f5577fa8be634da2937a3c}`)
   }
 }
